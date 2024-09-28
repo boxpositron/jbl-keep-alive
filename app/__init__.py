33d83f9b933fc5a1fd5b8a7b825bc7c74b7f082f
David Ibia <david.ibia@boxmarshall.com>
2024-09-28 20:58:35 +0100

    feat(__init__.py): add support for fetching and playing sound on default output device feat(__init__.py): add functionality to keep the active audio device alive by playing a sine wave feat(__init__.py): implement functionality to list audio devices and get the default output device

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
+from app.models import (
+    FrequencyConfiguration,
+    Device,
+    JBLDevice
+)
+from app.config import JBL_DEVICE_TARGET
+from app.devices import fetch_device_config
 import time
 import numpy as np
-import pyaudio
 import logging
-from app.models import FrequencyConfiguration, DeviceConfiguration
 
+import pyaudio
+import sounddevice as sd
+
+from typing import List, Dict
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,6 +22,10 @@ logging.basicConfig(
 
 
 logger = logging.getLogger(__name__)
+
+DeviceMap: Dict[str, JBLDevice] = {
+    JBL_DEVICE_TARGET: JBLDevice.CHARGE_3
+}
 
 
 def generate_sine_wave(
@@ -34,8 +47,8 @@ def play_sound(
     wave: np.ndarray,
     sample_rate: int
 ):
-    p = pyaudio.PyAudio()
 
+    p = pyaudio.PyAudio()
     # Open stream to play the sound
     stream = p.open(format=pyaudio.paFloat32,
                     channels=1,
@@ -51,23 +64,96 @@ def play_sound(
     p.terminate()
 
 
-def run_device(device: DeviceConfiguration):
+def list_audio_devices() -> List[Device]:
+    p = pyaudio.PyAudio()
 
-    logger.info(f"Running device: {device.device_name}")
+    devices: List[Device] = []
 
-    frequency_configuration = device.frequency_configuration
+    for i in range(p.get_device_count()):
+        device_info = p.get_device_info_by_index(i)
 
-    logger.info(
-        f"Generating sine wave at {frequency_configuration.frequency} Hz")
+        device = Device(
+            index=device_info["index"],
+            structVersion=device_info["structVersion"],
+            name=device_info["name"],
+            maxInputChannels=device_info["maxInputChannels"],
+            maxOutputChannels=device_info["maxOutputChannels"],
+            defaultLowInputLatency=device_info["defaultLowInputLatency"],
+            defaultLowOutputLatency=device_info["defaultLowOutputLatency"],
+            defaultHighInputLatency=device_info["defaultHighInputLatency"],
+            defaultHighOutputLatency=device_info["defaultHighOutputLatency"],
+            defaultSampleRate=device_info["defaultSampleRate"]
+        )
 
-    wave = generate_sine_wave(
-        frequency=frequency_configuration.frequency,
-        duration=frequency_configuration.duration,
-        sample_rate=frequency_configuration.sample_rate,
-        amplitude=frequency_configuration.amplitude
-    )
+        # Check if the device name is a string
+        if type(device.name) is str:
+            device.name = device.name.strip()
+
+        # Filter out non-output devices
+
+        if type(device.maxOutputChannels) is int:
+            if device.maxOutputChannels > 0:
+                devices.append(device)
+
+    p.terminate()
+
+    return devices
+
+
+def get_default_output_device() -> Device:
+    # Get the default output device
+    # Index 1 refers to the output device (0 for input)
+    default_output_index = sd.default.device[1]
+    device_info = sd.query_devices(default_output_index)
+
+    audio_devices = list_audio_devices()
+
+    selected_device = next(
+        (device for device in audio_devices if device.index ==
+         device_info["index"]),
+        None)
+
+    if not selected_device:
+        raise ValueError("No default output device")
+
+    return selected_device
+
+
+def keep_alive():
 
     while True:
+        active_device = get_default_output_device()
+
+        active_device_name = ""
+
+        if type(active_device.name) is str:
+            active_device_name = active_device.name.strip()
+
+        if not len(active_device_name):
+            logger.error("No active audio device found")
+            return
+
+        device_model = DeviceMap.get(active_device_name, None)
+
+        if not device_model:
+            logger.error(f"Device model not found for {active_device_name}")
+            return
+
+        device = fetch_device_config(device_model)
+
+        frequency_configuration = FrequencyConfiguration(
+            frequency=device.frequency_configuration.frequency,
+            duration=device.frequency_configuration.duration,
+            amplitude=device.frequency_configuration.amplitude,
+            sample_rate=active_device.defaultSampleRate,
+        )
+
+        wave = generate_sine_wave(
+            frequency=frequency_configuration.frequency,
+            duration=frequency_configuration.duration,
+            sample_rate=frequency_configuration.sample_rate,
+            amplitude=frequency_configuration.amplitude
+        )
 
         logger.info(f"Playing sound at {frequency_configuration.frequency} Hz")
         play_sound(wave=wave, sample_rate=frequency_configuration.sample_rate)
@@ -75,27 +161,14 @@ def run_device(device: DeviceConfiguration):
         logger.info(f"Sleeping for {device.interval} seconds")
         time.sleep(device.interval)
 
-# JBL Charge 3 - Device Configuration
-
-
-device = DeviceConfiguration(
-    device_name="JBL Charge 3",
-    interval=5,
-    frequency_configuration=FrequencyConfiguration(
-        frequency=10,  # 10 Hz can be used to keep this device awake
-        duration=2,
-        sample_rate=44100,
-        amplitude=0.5
-    )
-)
-
 
 def main() -> None:
     """
     Main function to run the application.
     """
     try:
-        run_device(device)
+
+        keep_alive()
 
     except KeyboardInterrupt:
         logger.info("Exiting application")
