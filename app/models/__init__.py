5ba12d098eb1e79594d4bbc973e01d7d9e1688a8
David Ibia <david.ibia@boxmarshall.com>
2024-09-28 20:58:20 +0100

    feat(models): add JBLDevice enum for JBL device models and Device model with various properties for configuration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,6 @@
+from enum import Enum
 from pydantic import BaseModel
+from typing import Union
 from typing_extensions import Annotated
 
 DEFAULT_SAMPLE_RATE: int = 44100
@@ -12,10 +14,30 @@ class FrequencyConfiguration(BaseModel):
     amplitude: Annotated[float, "Amplitude of the wave"] = DEFAULT_AMPLITUDE
 
 
+class JBLDevice(str, Enum):
+    CHARGE_3 = "JBL Charge 3"
+
+
 class DeviceConfiguration(BaseModel):
-    device_name: Annotated[str, "Name of the device"]
+    device_model: Annotated[JBLDevice, "Model of the device"]
     interval: Annotated[float, "Interval in seconds"]
     frequency_configuration: Annotated[
         FrequencyConfiguration,
         "Frequency configuration"
     ]
+
+
+DeviceValue = Union[str, int, float]
+
+
+class Device(BaseModel):
+    index: DeviceValue
+    structVersion: DeviceValue
+    name: DeviceValue
+    maxInputChannels: DeviceValue
+    maxOutputChannels: DeviceValue
+    defaultLowInputLatency: DeviceValue
+    defaultLowOutputLatency: DeviceValue
+    defaultHighInputLatency: DeviceValue
+    defaultHighOutputLatency: DeviceValue
+    defaultSampleRate: DeviceValue
