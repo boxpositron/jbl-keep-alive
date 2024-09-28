fd6c0b88728252622b181311302cf9cfc98d7f43
David Ibia <david.ibia@boxmarshall.com>
2024-09-28 21:30:58 +0100

    chore(lint.sh): update mypy command to include the flag --check-untyped-defs for stricter type checking during linting

--- lint.sh
+++ lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running linter"
-poetry run python -m mypy app
+poetry run python -m mypy app --check-untyped-defs
 
 echo "Running formatter"
 poetry run python -m pycodestyle app
